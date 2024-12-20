426d0301b4007e689bf18531d2210277f45dfad6
laasan <alekalx@gmail.com>
2024-12-21 00:47:20 +0530

    plot graph

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from taipy.gui import Icon
 from taipy import Config
 import datetime
 import pandas as pd
+import plotly.graph_objects as go
 
 stock_data = pd.read_csv("data/sp500_stocks.csv")
 company_data = pd.read_csv("data/sp500_companies.csv")
 country_names = company_data["Country"].unique().tolist()
-country_names = [(name, Icon("images/flags/" + name + ".png", name)) for name in country_names]
+country_names = [(name, Icon("images/flags/" + name + ".png", name))
+                 for name in country_names]
 company_names = company_data[
                     ["Symbol", "Shortname"]
                 ].sort_values("Shortname").values.tolist()
@@ -25,14 +27,14 @@ lin_pred = 0
 knn_pred = 0
 rnn_pred = 0
 
+graph_data = None
+figure = None
+
 # create page
 with tgb.Page() as page:
     with tgb.part("text-center"):
         tgb.image("images/icons/logo.png", width="10vw")
-        tgb.text(
-            "Stocker",
-            mode="md"
-        )
+        tgb.text("Stocker", mode="md")
         tgb.date_range(
             "{dates}",
             label_start="Start Date",
@@ -57,7 +59,7 @@ with tgb.Page() as page:
                 value_by_id=True
             )
 
-        tgb.chart()
+        tgb.chart(figure="{figure}")
         with tgb.part("text-left"):
             with tgb.layout("4 72 4 4 4 4 4 4"):
                 tgb.image(
@@ -84,26 +86,70 @@ with tgb.Page() as page:
                 )
                 tgb.text("{rnn_pred}")
 
+
 def build_company_names(country):
-    company_names = company_data[
+    comp_names = company_data[
         ["Symbol", "Shortname"]][
             company_data["Country"] == country
         ].sort_values("Shortname").values.tolist()
-    return company_names
+    return comp_names
 
-country_cfg = Config.configure_data_node(id="country")
-company_names_cfg = Config.configure_data_node(id="company_names")
+
+def build_graph_data(dates, company):
+    gr_data = stock_data[["Date", "Adj Close"]][
+        (stock_data["Symbol"] == company) &
+        (stock_data["Date"] > str(dates[0])) &
+        (stock_data["Date"] < str(dates[1]))  # сравнивать строки?
+    ]
+
+    gr_data = gr_data.rename(columns={"Adj Close": company})
+    return gr_data
+
+
+def display_graph(gr_data):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=gr_data["Date"],
+        y=gr_data[gr_data.columns[-1]],
+        name=gr_data.columns[-1],
+        showlegend=True
+        ))
+    fig.update_layout(
+        xaxis_title="Date",
+        yaxis_title="Stock Value"
+    )
+    return fig
+
+
+country_cfg = Config.configure_data_node(
+    id="country")
+company_names_cfg = Config.configure_data_node(
+    id="company_names")
+dates_cfg = Config.configure_data_node(
+    id="dates")
+company_cfg = Config.configure_data_node(
+    id="company")
+graph_data_cfg = Config.configure_data_node(
+    id="graph_data")
+
+build_graph_data_cfg = Config.configure_task(
+    input=[dates_cfg, company_cfg],
+    output=graph_data_cfg,
+    function=build_graph_data,
+    id="build_graph_data",
+    skippable=True)
+
 build_company_names_cfg = Config.configure_task(
     input=country_cfg,
     output=company_names_cfg,
     function=build_company_names,
     id="build_company_names",
-    skippable=True
-    )
+    skippable=True)
+
 scenario_cfg = Config.configure_scenario(
-    task_configs=build_company_names_cfg,
-    id="scenario"
-    )
+    task_configs=[build_company_names_cfg, build_graph_data_cfg],
+    id="scenario")
+
 
 def on_change(state, name, value):
     if name == "country":
@@ -113,6 +159,12 @@ def on_change(state, name, value):
         state.company_names = state.scenario.company_names.read()
     if name == "company":
         print(name, value)
+        state.scenario.dates.write(state.dates)
+        state.scenario.company.write(state.company)
+        state.scenario.submit(wait=True)
+        state.graph_data = state.scenario.graph_data.read()
+    if name == "graph_data":
+        state.figure = display_graph(state.graph_data)
 
 
 if __name__ == "__main__":
